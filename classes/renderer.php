61ee1c0c99a906a645fcfe287cfe58cc34ed0462
Lukas_Skywalker <lukas.diener@hotmail.com>
2020-10-05 22:15:30 +0200

    Move calendar code to renderer

--- classes/renderer.php
+++ classes/renderer.php
@@ -20,8 +20,6 @@ class renderer {
   }
 
   public function header() {
-    require_login();
-
     $title = get_string('global_nav_item', 'local_bbzcal');
     if($this->type == 'course') {
       $title = get_string('course_nav_item', 'local_bbzcal');
@@ -30,12 +28,71 @@ class renderer {
     $this->PAGE->set_context(\context_system::instance());
     $this->PAGE->set_pagelayout('standard');
     $this->PAGE->set_title($title);
-    $this->PAGE->set_url('/local/bbzcal/' . $this->type . '.php');
+    $this->PAGE->set_url('/local/bbzcal/calendar.php');
 
     $this->PAGE->set_heading($title);
     $this->PAGE->navbar->add($title);
 
     return $this->OUTPUT->header();
+  }
+
+  public function calendar($events) {
+    $str = '';
+    $today = new \DateTime();
+    $today->setTimezone(new \DateTimeZone('UTC'));
+    $dayOfMonth = $today->format('j');
+    $currentMonth = $today->format('n');
+    $daysInCurrentMonth = $today->format('t');
+
+    $date = (clone $today)->modify('first day of this month');
+    if ($date->format('w') != 1) {
+      $date->modify('previous monday');
+    }
+
+    $shouldStopRendering = false;
+
+    $str .= '<div class="local_bbzcal table">';
+
+    $weekdays = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
+    foreach($weekdays as $weekday) {
+      $str .= '<div class="dayhead">' . $weekday . '</div>';
+    }
+
+    while (!$shouldStopRendering) {
+      $weekDay = $date->format('w');
+      $month = $date->format('n');
+      $day = $date->format('j');
+
+      if ($month != $currentMonth) {
+        // we're either at the beginning or end of our table
+        $str .= '<div class="day">';
+      } else if ($day == $dayOfMonth) {
+        // highlight the current date
+        $str .= '<div class="day current">' . $day;
+      } else {
+        $str .= '<div class="day">' . $day;
+      }
+
+      $matches = array_filter($events, function ($k) use ($date) {
+        return $k->date == $date->getTimestamp();
+      });
+      foreach($matches as $match) {
+        $str .= '<div class="event">' . $match->title . '</div>';
+      }
+
+      $str .= '</div>';
+
+      if ($weekDay == 0) {
+        if ($month != $currentMonth || $day == $daysInCurrentMonth) {
+            $shouldStopRendering = true;
+        }
+      }
+
+      // move on to the next day we need to display
+      $date->modify('+1 day');
+    }
+    $str .= '</div>';
+    return $str;
   }
 
   public function footer() {
